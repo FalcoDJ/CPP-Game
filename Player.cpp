52211de426587c15704e51d3ee0010df99d81688
Isaac's Computer <cuser@pop-os.localdomain>
2020-08-09 19:50:03 -0400

    Added Stop Motion Variables

--- Player.cpp
+++ Player.cpp
@@ -10,26 +10,52 @@ Player::Player()
   m_Sprite.setTexture(TextureHolder::GetTexture("img/Player/sprite.png"));
 }
 
+//Moving Functions
+//move left
 void Player::moveLeft()
 {
   leftKey = true;
 }
 
+//move right
 void Player::moveRight()
 {
   rightKey = true;
 }
 
+//Jump
 void Player::Jump()
 {
   jumpKey = true;
 }
 
+//Crouch
 void Player::Crouch()
 {
   downKey = true;
 }
 
+//Stop Motion Variables
+
+//stop left
+void Player::stopLeft()
+{
+  leftKey = false;
+}
+
+//stop right
+void Player::stopRight()
+{
+  rightKey = false;
+}
+
+//stand / stop crouching
+void Player::Stand()
+{
+  downKey = true;
+}
+
+//Other Variables
 Sprite Player::getSprite()
 {
   return m_Sprite;
--- Player.h
+++ Player.h
@@ -43,6 +43,7 @@ class Player
 
     void stopLeft();
     void stopRight();
+    void Stand();
 
     void update(float elapsedTime);
 
