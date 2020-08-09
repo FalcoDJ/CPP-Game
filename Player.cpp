bfce4c40a4673c2a4d4370c6a68a54ab3f0b6de4
Isaac's Computer <cuser@pop-os.localdomain>
2020-08-09 13:42:34 -0400

    Draw dog on screen

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,21 @@
 #include "Player.h"
+#include "TextureHolder.h"
+
+Player::Player()
+{
+  //Starter Stuff
+  m_Health = 100;
+
+  //Texture and Sprite
+  m_Sprite.setTexture(TextureHolder::GetTexture("img/Player/sprite.png"));
+}
+
+Sprite Player::getSprite()
+{
+  return m_Sprite;
+}
+
+FloatRect Player::getPosition()
+{
+  return m_Sprite.getGlobalBounds();
+}
