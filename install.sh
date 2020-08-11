c2071f352438d0702bbb7793e4cf77ffc5cba4c9
Isaac's Computer <cuser@pop-os.localdomain>
2020-08-11 15:42:50 -0400

    Created CMakeLists.txt and installer

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+mkdir build
+cd build
+cmake ..
+make
+cp GameLauncher ..
+cd ..
+rm -r build
+./GameLauncher
