dcf70d5b7cdce0bf5e860e18149aba86c7ea27e1
Isaac's Computer <cuser@pop-os.localdomain>
2020-08-13 17:05:43 -0400

    Changes to collision funcs.

--- Collisions.cpp
+++ Collisions.cpp
@@ -3,13 +3,20 @@
 
 using namespace sf;
 
-bool CollisionY(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
+bool CollisionBottom(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
 {
   if (Obj1.y <= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) >= Obj2.y)
   {
     return true;
   }
-  else if  (Obj1.y >= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) <= Obj2.y)
+  else
+  {
+    return false;
+  }
+}
+bool CollisionTop(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
+{
+  if  (Obj1.y >= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) <= Obj2.y)
   {
     return true;
   }
--- Collisions.h
+++ Collisions.h
@@ -4,4 +4,6 @@ using namespace sf;
 
 enum class Side {Left, Right, Top, Bottom};
 
-bool CollisionY(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
+bool CollisionBottom (Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
+
+bool CollisionTop(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
