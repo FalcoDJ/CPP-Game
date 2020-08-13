c8f3670dff151fa226f2cedae05a7536d7b4d0b2
Isaac's Computer <cuser@pop-os.localdomain>
2020-08-13 17:54:08 -0400

    Added more collision funcs.

--- Collisions.cpp
+++ Collisions.cpp
@@ -3,25 +3,73 @@
 
 using namespace sf;
 
-bool CollisionBottom(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
+bool CollisionsGround(Vector2f Obj1, int Obj1Height, Vector2f Obj2)
 {
-  if (Obj1.y <= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) >= Obj2.y)
+  if (Obj1.y >= Obj2.y - Obj1Height)
   {
     return true;
   }
   else
-  {
     return false;
-  }
+}
+
+//Y coords
+
+bool CollisionY(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
+{
+  if (CollisionBottom(Obj1, Obj1Height, Obj2, Obj2Height)
+   || CollisionTop(Obj1, Obj1Height, Obj2, Obj2Height))
+   {
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+}
+
+bool CollisionBottom(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
+{
+  if (Obj1.y <= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) >= Obj2.y)
+    return true;
+  else
+    return false;
 }
 bool CollisionTop(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height)
 {
   if  (Obj1.y >= (Obj2.y + Obj2Height) && (Obj1.y + Obj1Height) <= Obj2.y)
-  {
     return true;
-  }
   else
-  {
     return false;
-  }
+}
+
+
+//X coords
+
+bool CollisionX(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width)
+{
+  if (CollisionLeft(Obj1, Obj1Width, Obj2, Obj2Width)
+   || CollisionRight(Obj1, Obj1Width, Obj2, Obj2Width))
+   {
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+}
+
+bool CollisionLeft(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width)
+{
+  if (Obj1.x <= (Obj2.x + Obj2Width) && (Obj1.x + Obj1Width) >= Obj2.x)
+    return true;
+  else
+    return false;
+}
+bool CollisionRight(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width)
+{
+  if  (Obj1.x >= (Obj2.x + Obj2Width) && (Obj1.x + Obj1Width) <= Obj2.x)
+    return true;
+  else
+    return false;
 }
--- Collisions.h
+++ Collisions.h
@@ -4,6 +4,14 @@ using namespace sf;
 
 enum class Side {Left, Right, Top, Bottom};
 
-bool CollisionBottom (Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
+bool CollisionsGround(Vector2f Obj1, int Obj1Height, Vector2f Obj2);
 
+bool CollisionY(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
+
+bool CollisionBottom(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
 bool CollisionTop(Vector2f Obj1, int Obj1Height, Vector2f Obj2, int Obj2Height);
+
+bool CollisionX(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width);
+
+bool CollisionLeft(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width);
+bool CollisionRight(Vector2f Obj1, int Obj1Width, Vector2f Obj2, int Obj2Width);
